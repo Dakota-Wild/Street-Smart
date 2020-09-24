a245d015990a5d5a8ce6b86cd9ff3b8152ba3426
Darrell Wong <theredpok@gmail.com>
2020-09-23 23:10:27 -0700

    Added Math3 dependency and random method

--- demo/demo/src/main/java/com/dakotawild/demo/HomeController.java
+++ demo/demo/src/main/java/com/dakotawild/demo/HomeController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.commons.math3.random.MersenneTwister;
+
 @RequestMapping
 @RestController
 public class HomeController {
-	
+
 	@GetMapping("/Andrew_URL")
 	public String getURLandrew() {
 		return "Lakers 2020 NBA Champions confirmed :P";
@@ -22,14 +24,23 @@ public class HomeController {
 	public String getURLDarrell() {
 		return "im still so confused";
 	}
-	
+
 	@GetMapping("/Dakota_URL")
 	public String getURLDakota() {
 		return "This is whack dot com";
 	}
-	
+
 	@GetMapping("/Celine_URL")
 	public String getURLCeline() {
 		return "What is school?";
+	}
+
+	@GetMapping("/math")
+	public String randomScore() {
+
+		MersenneTwister rng = new MersenneTwister();
+		int num = rng.nextInt() % 100;
+		String result = "Your random number from (-100 to 100) is: " + num;
+		return result;
 	}
 }
