1355746376a48e4831a0393d9499eabcb0f1693a
CelineTran <celinetran7737@gmail.com>
2020-09-23 23:27:18 -0700

    update ical4j

--- demo/demo/src/main/java/com/dakotawild/demo/WebController.java
+++ demo/demo/src/main/java/com/dakotawild/demo/WebController.java
@@ -12,14 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import net.fortuna.ical4j.model.Calendar; 
 import net.fortuna.ical4j.model.component.VEvent; 
 import net.fortuna.ical4j.model.PropertyList; 
-import net.fortuna.ical4j.model.property.ProdId; 
-import net.fortuna.ical4j.model.property.Version; 
-import net.fortuna.ical4j.model.property.CalScale; 
-import net.fortuna.ical4j.model.property.Location; 
-import net.fortuna.ical4j.model.property.Duration; 
-import net.fortuna.ical4j.model.TimeZone; 
-import net.fortuna.ical4j.model.DateTime; 
-import java.util.GregorianCalendar; 
+import net.fortuna.ical4j.model.property.Uid; 
+import net.fortuna.ical4j.model.Date; 
 
 @RequestMapping
 @RestController
@@ -37,27 +31,17 @@ public class WebController {
     
     @GetMapping("/ical4j") 
     public Calendar myCalendar(){
-	    TimeZone tz = Utils.getTimezone("America/New_York"); 
-	    java.util.Calendar startDate = new GregorianCalendar(); 
-	    startDate.setTimeZone(tz);
-	    startDate.add(java.util.Calendar.DAY_OF_MONTH, 9); 
-	    startDate.set(java.util.Calendar.MINUTE, 0); 
-	    startDate.set(java.util.Calendar.SECOND, 0); 
+	    java.util.Calendar calendar = java.util.Calendar.getInstance(); 
+	    calendar.set(java.util.Calendar.Month, java.util.Calendar.JANUARY); 
+	    calendar.set(java.util.Calendar.DAY_OF_MONTH, 1); 
+	
+	    VEvent newYears = new VEvent(new Date(calendar.getTime()), "New Years"); 
+	    UidGenerator ug = new UidGeneartor("1"); 
+	    newYears.getProperties().add(ug.generatedUid()); 
 	    
-	    String eventName = "CS4800"; 
-	    DateTime start = new DateTime(startDate.getTime()); 
-	    start.setTimeZone(tz); 
+	    Calendar cal = new Calendar(); 
+	    cal.getComponents.add(newYears); 
 	    
-	    Calendar calendar = new Calendar(); 
-	    calendar.getProperties().add(new ProdId("-//Celine Tran//iCal4j 1.0//EN")); 
-	    calendar.getProperties().add(Version.VERSION_2_0); 
-	    calendar.getProperties().add(CalScale.GREGORIAN); 
-	    VEvent vEvent = new VEvent(start, eventName); 
-	    vEvent.getProperties().add(new Uid("CS4800"));
-	    vEvent.getProperties().add(new Duration(null, "1H15M")); 
-	    vEvent.getProperties().add(new Location("Zoom")); 
-	    calendar.getComponents().add(vEvent); 
-
-	    return calendar; 
+	    return cal; 
     }
 }
