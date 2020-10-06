a62064a0ca39b2ee1cc8d84b01351edbd4a6f8d9
Kobe Lee <kobemhlee@gmail.com>
2020-10-06 10:53:00 -0700

    Added JUnit Test for JMoney Class

--- demo/demo/src/test/java/com/dakotawild/demo/DemoApplicationTests.java
+++ demo/demo/src/test/java/com/dakotawild/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.dakotawild.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
 
-@SpringBootTest
+
 class DemoApplicationTests {
 
-	@Test
+
 	void contextLoads() {
+	}
+
+	@Test
+	public void testJMoney(){
+		Money exampleMoney = Money.parse("USD 58.23");
+		CurrencyUnit usd = CurrencyUnit.of("USD");
+		money = money.plus(Money.of(usd, 12.43d));
+		String result = money.toString();
+		Assert.assertEquals("$70.46", result);
 	}
 
 }
