8ff9b4d33e7527171272f50b8990b36e86d3430d
Dakota Eckheart <deckheart@cpp.edu>
2020-11-03 21:41:00 -0800

    New directories

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Street-Smart Read-me
+# Street-Smart
+## A scheduling and routing web service tailored to the commuter.
+---------------
+Street-Smart is designed for those who have a schedule and places to be. Street-Smart allows for users to input their own meetings, classes, and appointments, along with their respective locations, to create a schedule. Users receive notifications on the best times to start their commute based on the times and places listed in their schedule.
---
+++ backend/src/main/java/com/dakotawild/demo/HomeController.java
@@ -0,0 +1,40 @@
+package com.dakotawild.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping
+@RestController
+public class HomeController {
+	@GetMapping(" ")
+	public String home() {
+		return "Dakota Wild Home Page";
+	}
+
+	@GetMapping("/Andrew_URL")
+	public String getURLandrew() {
+		return "Lakers 2020 NBA Champions confirmed :P";
+	}
+
+	@GetMapping("/Kobe_URL")
+	public String getURLKobe() {
+		return "Week 5 yet done with school";
+	}
+
+	@GetMapping("/Darrell_URL")
+	public String getURLDarrell() {
+		return "im still so confused";
+	}
+
+	@GetMapping("/Dakota_URL")
+	public String getURLDakota() {
+		return "This is whack dot com";
+	}
+
+	@GetMapping("/Celine_URL")
+	public String getURLCeline() {
+		return "What is school?";
+	}
+}
