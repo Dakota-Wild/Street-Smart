479ba2f0102e32f538109f46643362755a71b510
kobemhlee <kobemhlee@gmail.com>
2020-11-15 23:19:00 -0800

    Added Repository for Mongodb

--- backend/src/main/java/com/dakotawild/streetsmart/User.java
+++ backend/src/main/java/com/dakotawild/streetsmart/User.java
@@ -1,25 +1,30 @@
 package com.dakotawild.streetsmart;
-
 import org.springframework.data.annotation.Id;
 
+@Document(collection="Users")
 public class User {
-    @Id
-    public String id;
 
-    public String firstName;
-    public String lastName;
+  @Id
+  public String id;
+ 
+  public String firstName;
+  public String lastName;
+  public String email;
+  public String password;
 
-    public User() {}
+  public User() {}
 
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  public User(String firstName, String lastName, String email, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = passsword;
+  }
 
-    @Override
-    public String toString() {
-        return String.format(
-            "User[id=%s, firstName='%s', lastName='%s']",
-            id, firstName, lastName);
-    }
+  @Override
+  public String toString() {
+    return String.format(
+        "User[id=%s, firstName='%s', lastName='%s', email='%s', password='%s']",
+        id, firstName, lastname, email, password);
+  }
 }
--- backend/src/main/java/com/dakotawild/streetsmart/UserRepository.java
+++ backend/src/main/java/com/dakotawild/streetsmart/UserRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
     
-    public User findByFirstName(String firstName);
+    public List<User> findByFirstName(String firstName);
     public List<User> findByLastName(String lastName);
+    public List<User> findByEmail(String email);
+    public List<User> findByPassword(String password);
     
 }
