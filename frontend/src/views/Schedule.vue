8c48e53f05c04081b1c38ba667b2891ac9a8be47
Andrew Kim <akim1@cpp.edu>
2020-11-18 00:09:49 -0800

    Removed old signup form from Schedule

--- frontend/src/views/Schedule.vue
+++ frontend/src/views/Schedule.vue
@@ -26,56 +26,12 @@
           </div>
         </template>
       </vc-calendar>
-      <form action="action_page.php">
-        <div class="container">
-          <h1>Add New Event</h1>
-          <p>Please fill in this form to create a new event.</p>
-          <hr />
-
-          <label for="eventName"><b>Event Name</b></label>
-          <input
-            type="text"
-            placeholder="Enter Event Name"
-            name="eventName"
-            id="eventName"
-            required
-          />
-          <br />
-
-          <label for="=startTime"><b>Event Date</b></label>
-          <date-pick v-model="date" :format="'YYYY.MM.DD'"></date-pick>
-
-          <label for="=startTime"><b>Start Time</b></label>
-          <vue-timepicker format="h:m a"></vue-timepicker>
-
-          <label for="endTime"><b>End Time</b></label>
-          <vue-timepicker format="h:m a"></vue-timepicker>
-          <br />
-
-          <label for="address"><b>Address</b></label>
-          <input
-            type="text"
-            placeholder="Enter Full Address"
-            name="address"
-            id="address"
-            required
-          />
-
-          <button type="submit" class="registerbtn">Add</button>
-        </div>
-      </form>
     </div>
   </div>
 </template>
 
 <script>
-import VueTimepicker from "vue2-timepicker";
-import DatePick from "vue-date-pick";
-import "vue2-timepicker/dist/VueTimepicker.css";
 export default {
-  name: "app",
-  components: { VueTimepicker, DatePick },
-
   data() {
     const month = new Date().getMonth();
     const year = new Date().getFullYear();
@@ -229,59 +185,6 @@ button {
   }
   * {
     box-sizing: border-box;
-  }
-
-  /* Add padding to containers */
-  .container {
-    padding: 16px;
-  }
-
-  /* Full-width input fields */
-  input[type="text"] {
-    width: 100%;
-    padding: 15px;
-    margin: 5px 0 22px 0;
-    display: inline-block;
-    border: none;
-    background: #f1f1f1;
-  }
-
-  input[type="text"]:focus {
-    background-color: #ddd;
-    outline: none;
-  }
-
-  /* Overwrite default styles of hr */
-  hr {
-    border: 1px solid #f1f1f1;
-    margin-bottom: 25px;
-  }
-
-  /* Set a style for the submit/register button */
-  .registerbtn {
-    background-color: #4caf50;
-    color: white;
-    padding: 16px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-    opacity: 0.9;
-  }
-
-  .registerbtn:hover {
-    opacity: 1;
-  }
-
-  /* Add a blue text color to links */
-  a {
-    color: dodgerblue;
-  }
-
-  /* Set a grey background color and center the text of the "sign in" section */
-  .signin {
-    background-color: #f1f1f1;
-    text-align: center;
   }
 }
 </style>
