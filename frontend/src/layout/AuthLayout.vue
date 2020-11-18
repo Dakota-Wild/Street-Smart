ad8f31e61d77638fcd44debd6d4460435d01867d
kobemhlee <kobemhlee@gmail.com>
2020-11-18 00:15:23 -0800

    adjusted user

--- frontend/src/layout/AuthLayout.vue
+++ frontend/src/layout/AuthLayout.vue
@@ -31,10 +31,6 @@
             <div class="container">
                 <div class="row align-items-center justify-content-xl-between">
                     <div class="col-xl-6">
-                        <div class="copyright text-center text-xl-left text-muted">
-                            &copy; {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1"
-                                               target="_blank">Creative Tim</a>
-                        </div>
                     </div>
                 </div>
             </div>
